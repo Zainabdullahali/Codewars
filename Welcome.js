943a9d8941aad891266bc61602f222727b149728
zainabdullahali <zainabdullahali1810@gmail.com>
2023-09-12 00:07:17 +0530

    12-09-23

---
+++ Welcome.js
@@ -0,0 +1,26 @@
+function greet(language) {
+  const greeting = {
+    english: "Welcome",
+    czech: "Vitejte",
+    danish: "Velkomst",
+    dutch: "Welkom",
+    estonian: "Tere tulemast",
+    finnish: "Tervetuloa",
+    flemish: "Welgekomen",
+    french: "Bienvenue",
+    german: "Willkommen",
+    irish: "Failte",
+    italian: "Benvenuto",
+    latvian: "Gaidits",
+    lithuanian: "Laukiamas",
+    polish: "Witamy",
+    spanish: "Bienvenido",
+    swedish: "Valkommen",
+    welsh: "Croeso",
+  };
+  if (greeting[language]) {
+    return greeting[language];
+  } else {
+    return "Welcome";
+  }
+}
