7e3358df68c669c13e502cf9ed05e04e9d1bf9cd
zainabdullahali <zainabdullahali1810@gmail.com>
2023-09-18 23:14:16 +0530

    18-09-23

---
+++ 8kyu/Are_they_opposite.js
@@ -0,0 +1,8 @@
+function isOpposite(s1, s2) {
+  return (
+    s1
+      .split("")
+      .map((c) => (c === c.toUpperCase() ? c.toLowerCase() : c.toUpperCase()))
+      .join("") === s2 && s1 !== ""
+  );
+}
