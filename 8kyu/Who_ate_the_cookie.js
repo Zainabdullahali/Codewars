ae1872485a2dc1572b9c4d26b3e5a19277217d59
zainabdullahali <zainabdullahali1810@gmail.com>
2023-10-02 00:52:25 +0530

    02-10-23

---
+++ 8kyu/Who_ate_the_cookie.js
@@ -0,0 +1,13 @@
+function cookie(x) {
+  if (typeof x === "string") {
+    return "Who ate the last cookie? It was Zach!";
+  }
+  if (typeof x === "number") {
+    return "Who ate the last cookie? It was Monica!";
+  }
+  if (typeof x === "number" && !Number.isInteger(x)) {
+    return "Who ate the last cookie? It was Monica!";
+  } else {
+    return "Who ate the last cookie? It was the dog!";
+  }
+}
